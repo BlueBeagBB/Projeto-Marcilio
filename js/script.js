9253c122699a718f4e652cd513988ccc86d051b0
BlueBeag <gabrifelipegf@gmail.com>
2024-10-18 12:44:02 -0300

    css responsivo incompleto

--- js/script.js
+++ js/script.js
@@ -4,11 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const flashcardsContainer = document.getElementById("flashcardsContainer");
 
     addElementButton.addEventListener("click", function () {
-        // Cria o container do flashcard
         const newContainer = document.createElement("div");
         newContainer.classList.add("container");
 
-        // Botão de fechar o flashcard
         const closeButton = document.createElement("button");
         closeButton.classList.add("close-button");
         closeButton.innerHTML = "&times;";
@@ -17,31 +15,26 @@ document.addEventListener("DOMContentLoaded", function () {
         });
         newContainer.appendChild(closeButton);
 
-        // Elemento interno para o conteúdo do flashcard
         const innerElement = document.createElement("div");
         innerElement.classList.add("inner-element");
 
-        // Input para o nome do flashcard/postagem
         const nameInput = document.createElement("input");
         nameInput.setAttribute("type", "text");
         nameInput.setAttribute("placeholder", "Título da Postagem");
         nameInput.classList.add("container-name");
         innerElement.appendChild(nameInput);
 
-        // Textarea para o conteúdo do post
         const contentInput = document.createElement("textarea");
         contentInput.setAttribute("placeholder", "Escreva seu texto aqui...");
         contentInput.classList.add("content-input");
         innerElement.appendChild(contentInput);
 
-        // Input para upload de imagem
         const imageInput = document.createElement("input");
         imageInput.setAttribute("type", "file");
         imageInput.setAttribute("accept", "image/*");
         imageInput.classList.add("image-input");
         innerElement.appendChild(imageInput);
 
-        // Botão para criar o flashcard/postagem
         const createButton = document.createElement("button");
         createButton.textContent = "Publicar Postagem";
         createButton.addEventListener("click", function () {
@@ -63,7 +56,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 postContent.textContent = content;
                 customContainer.appendChild(postContent);
 
-                // Se uma imagem foi carregada, exibe-a
                 if (imageFile) {
                     const imgElement = document.createElement("img");
                     imgElement.src = URL.createObjectURL(imageFile);
